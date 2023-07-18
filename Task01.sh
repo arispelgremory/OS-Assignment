bc098ff34c3488ca29a22b5923cfd16ed5884dc1
gremoryyx <arispelgremory@gmail.com>
2023-07-18 13:32:08 +0800

    task 1 almost done

--- Task01.sh
+++ Task01.sh
@@ -3,4 +3,38 @@
 
 bold=$(tput bold)
 normal=$(tput sgr0)
-echo "${bold}Hello, $PERSON!${normal}" 
+
+
+echo "${bold}University Venue Management Menu${normal}\n" 
+echo "A – Register New Patron\nB – Search Patron Details\nC – Add New Venue\nD – List Venue\nE – Book Venue"
+echo "\nQ – Exit from Program"
+
+echo "\nPlease select a choice: "
+read choice
+
+echo You have selected ${choice}
+
+
+case $choice | tr '[a-z]' '[A-Z]' in 
+    "A")
+        echo "Register New Patron"
+        ;;
+    "B")
+        echo "Search Patron Details"
+        ;;
+    "C")
+        echo "Add New Venue"
+        ;;
+    "D")
+        echo "List Venue"
+        ;;
+    "E")
+        echo "Book Venue"
+        ;;
+    "Q")
+        echo "Exit from Program"
+        ;;
+    *)
+        echo "Invalid Choice"
+        ;;
+esac
