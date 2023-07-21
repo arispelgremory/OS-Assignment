3f685707f9ea6e38b1fcfeb600842c1fc99b1d46
shiyoruxz <shiyoruxz@gmail.com>
2023-07-21 12:06:45 +0800

    Looping bug solved

--- Task02.sh
+++ Task02.sh
@@ -1,24 +1,3 @@
-# Register Patron
-registerPatron() {
-    # patronList = patron.txt
-
-    echo "Patron Registration"
-    echo "==================="
-
-    read -p "Patron ID (As per TAR UMT format): " patronID
-    read -p "Patron Full Name (As per NRIC): " patronName
-    read -p "Contact Number: " contactNumber
-    read -p "Email Address (As per TAR UMT format): " email
-    echo -e "$patronID:$patronName:$contactNumber:$email" >> ".\patron.txt"
-    echo
-
-    read -p "Register Another Patron? (y)es or (q)uit: " newRegistration
-    echo
-
-    echo "Press (q) to return to University Venue Management Menu." 
-    read mainMenu
-}
-
 # Center Algin Text
 centerAlign() {
     local text="$1"
@@ -33,101 +12,107 @@ centerAlign() {
     printf "%${spaces_before}s%s%${spaces_after}s\n" "" "$text" ""
 }
 
+# Register Patron
+registerPatron() {
+    # patronList = patron.txt
+
+    echo "Patron Registration"
+    echo "==================="
+
+    read -p "Patron ID (As per TAR UMT format): " patronID
+    read -p "Patron Full Name (As per NRIC): " patronName
+    read -p "Contact Number: " contactNumber
+    read -p "Email Address (As per TAR UMT format): " email
+    echo -e "$patronID:$patronName:$contactNumber:$email" >> ".\patron.txt\n"
+
+    echo read -p "Register Another Patron? (y)es or (q)uit: " newRegistration 
+    
+
+    echo "Press (q) to return to University Venue Management Menu." 
+    read mainMenu
+}
+
 # Search Patron
 searchPatron() {
-    # Pre_ifs="$IFS"          
-    # IFS=":"
-    # for Per in $Persons
-    # do
-    #     echo  The name of Persons are $Per
-    # done
-
-
     centerAlign "Search Patron Details"
-    centerAlign "_____________________"
+    centerAlign "____________________"
     echo
 
-    read -p "Enter Patron ID:" readID
+    # read data from patron.txt
+    read -p "Enter Patron ID to search: " patronID 
     echo
-
-    # IFS=":" read -ra patronDetailsArray < patron.txt
-
-    for i in {1..100}
-    do
-        printf "_"
-    done
-    echo
-
-    echo "Full Name: " $patronName
-    echo "Contact Number: " $contactNumber
-    echo "Email Address (As per TAR UMT format) :"
-    echo
-
-    echo "Search Another Person? (y)es or (q)uit :"
-    echo
-
-    echo "Press (q) to return to University Venue Management Menu."
-}
-
-IFSTesting() {
-    Persons=Alex:Thomas:Kyle:Jenny
-    Pre_ifs="$IFS"          
-    IFS=":"
-    for Per in $Persons
-    do
-        echo The name of Persons are $Per
-    done
-}
-
-textFile() {
-    patronID="1234"
 
     readarray -t patronList < patron.txt
     IFS=$'\n'
-
-    check=false
-    patronDetailsArray=()
 
     counter=0
     for getPatronList in ${patronList[@]}; do
         ((counter++))
     done
 
-    arrayCounter=0
-    
-    # while [ $check==false ]; do
-            # for getPatronList in ${patronList[counter]}; do
-            for (( i=1; i<=$counter; i++ )); do
-                echo $i
-                for perItem in ${patronList[arrayCounter]}; do
-                    IFS=$':'
+    for (( i=0; i<$counter; i++ )); do
+        IFS=$'\n'
+        for perItem in ${patronList[arrayCounter]}; do              
+            IFS=$':'
+            read -ra patronDetailsArray <<< $perItem
 
-                    read -ra patronDetailsArray <<< $perItem
-                    echo "This is the ID: " ${patronDetailsArray[0]}
-                    echo "perItem: "$perItem
-                    echo "Whole List: "${patronList[arrayCounter]}
+            if [ ${patronDetailsArray[0]} != $patronID ]; then  
+                (( arrayCounter++ ))
+            else
+                arrayCounter=$arrayCounter
+                (( i=3 ))
+                # echo "Patron Details:" ${patronDetailsArray[@]}
+                break
+            fi
+        done
+    done  
 
-                    if [ ${patronDetailsArray[0]}==$patronID ]; then
-                        unset patronDetailsArray
-                        ((arrayCounter++))
-                        echo "patronID:" $patronID
-                        echo "arrayCounter:" $arrayCounter
-                        echo "false"
-                        echo
+    # display Patron details according patronID
+    for i in {1..100}
+    do
+        printf "_"
+    done
+    echo
 
-                        # if [ $i==3 ]; then
-                        #     check=true
-                        #     echo "error"
-                        # fi
-                    else
+    echo "Full Name:" ${patronDetailsArray[1]}
+    echo "Contact Number:" ${patronDetailsArray[2]}
+    echo -e "Email Address (As per TAR UMT format):" ${patronDetailsArray[3]} "\n"
 
-                        (( i=3 ))
-                        check=true
-                        echo "true"
-                        echo
-                    fi
-                done
-            done  
-    # done 
+    # read "Search Another Person? (y)es or (q)uit :"
+
+    # read "Press (q) to return to University Venue Management Menu."
 }
-textFile
+
+searchPatron
+
+readTextfile() {
+    patronID=230812
+    # echo read -p "Enter Patron ID to search: " patronID
+
+    readarray -t patronList < patron.txt
+    IFS=$'\n'
+
+    counter=0
+    for getPatronList in ${patronList[@]}; do
+        ((counter++))
+    done
+
+    for (( i=0; i<$counter; i++ )); do
+        IFS=$'\n'
+
+        for perItem in ${patronList[arrayCounter]}; do              
+            IFS=$':'
+            read -ra patronDetailsArray <<< $perItem
+
+            if [ ${patronDetailsArray[0]} != $patronID ]; then  
+                (( arrayCounter++ ))
+            else
+                arrayCounter=$arrayCounter
+                (( i=3 ))
+                echo "Patron Details:" ${patronDetailsArray[@]}
+                break
+            fi
+        done
+    done  
+}
+# textFile
